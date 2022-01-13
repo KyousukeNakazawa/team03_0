37a0f8db0cffc3eb2789bd5d1e9fe5a8f97a3c52
kyousukenakazawa <k021g1400@g.neec.ac.jp>
2022-01-13 23:07:21 +0900

    スコア保存機能の実装

---
+++ score.cpp
@@ -0,0 +1,68 @@
+#include "DxLib.h"
+#include "score.h"
+#include "enum.h"
+
+Score::Score() {
+	rfp = NULL;
+	wfp = NULL;
+	LoadDivGraph("resource/pict/scorenum.png", 10, 10, 1, 48, 48, numberGH);
+}
+
+void Score::Ranking(int score){
+	error = fopen_s(&rfp, "resource/save.txt", "r");
+
+	int digitCount = 0;// 何行読み込んだか
+	while (fgets(data, 100, rfp) != NULL)
+	{
+		int tmp = atoi(data);// ascii to int
+		arr[digitCount] = tmp;
+		digitCount++;
+	}
+
+	fclose(rfp);// 閉じる
+	// ---------- スコアを更新 ----------
+	if (arr[0] < score)
+	{
+		arr[2] = arr[1];
+		arr[1] = arr[0];
+		arr[0] = score;
+	}
+	else if (arr[1] < score && arr[0] > score)
+	{
+		arr[2] = arr[1];
+		arr[1] = score;
+	}
+	else if (arr[2] < score && arr[1] > score)
+	{
+		arr[2] = score;
+	}
+	// ---------- ファイルに更新した値を書き込む(write) ----------
+	error = fopen_s(&wfp, "resource/save.txt", "w");
+
+	// ファイルに書き込み
+	for (int i = 0; i < 3; ++i)
+	{
+		fprintf(wfp, "%d\n", arr[i]);
+	}
+
+	fclose(wfp);// 閉じる
+
+	//スコアランキング描画
+	sprintf_s(strNumSave1, sizeof(strNumSave1), "%05d", arr[0]);
+	sprintf_s(strNumSave2, sizeof(strNumSave2), "%05d", arr[1]);
+	sprintf_s(strNumSave3, sizeof(strNumSave3), "%05d", arr[2]);
+
+	//オフセット値に合わせる
+	for (int i = 0; i < digits; i++) {
+		eachNumSave1[i] = strNumSave1[i] - 48;
+		eachNumSave2[i] = strNumSave2[i] - 48;
+		eachNumSave3[i] = strNumSave3[i] - 48;
+	}
+
+	//スコア
+	for (int i = 0; i < digits; i++) {
+		DrawGraph(48 * i + 700, 515, numberGH[eachNumSave1[i]], true);
+		DrawGraph(48 * i + 700, 570, numberGH[eachNumSave2[i]], true);
+		DrawGraph(48 * i + 700, 625, numberGH[eachNumSave3[i]], true);
+	}
+}
---
+++ score.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "screen.h"
+
+class Score {
+private:
+	//スコアのセーブ
+	FILE* rfp;
+	FILE* wfp;
+
+	int error;// ファイルを読み込めたか
+	char data[100];// ファイルから読み込んだデータ(文字列)
+	int arr[3];// ファイルから読み込んだデータ(整数型)
+
+	const int digits = 6;
+
+	char strNumSave1[6] = { 0 };
+	int eachNumSave1[6] = { 0 };
+
+	char strNumSave2[6] = { 0 };
+	int eachNumSave2[6] = { 0 };
+
+	char strNumSave3[6] = { 0 };
+	int eachNumSave3[6] = { 0 };
+
+	int numberGH[10];
+
+public:
+	Score();
+
+	void Ranking(int score);
+};
